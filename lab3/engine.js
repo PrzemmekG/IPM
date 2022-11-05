4340271071db2447a065b45ec960a8245e1f5971
PrzemekG <przemek@gabrych.com.pl>
2022-11-05 18:33:08 +0100

    Some changes in HTML

--- lab3/engine.js
+++ lab3/engine.js
@@ -1,39 +1,55 @@
-let items = document.querySelectorAll('#items-list > li')
-
-items.forEach(item => {
-  $(item).prop('draggable', true)
-  item.addEventListener('dragstart', dragStart)
-  item.addEventListener('drop', dropped)
-  item.addEventListener('dragenter', cancelDefault)
-  item.addEventListener('dragover', cancelDefault)
-})
-
-function dragStart (e) {
-  var index = $(e.target).index()
-  e.dataTransfer.setData('text/plain', index)
-}
-
-function dropped (e) {
-  cancelDefault(e)
+function slist (target) {
+    // (A) SET CSS + GET ALL LIST ITEMS
+    target.classList.add("slist");
+    let items = target.getElementsByTagName("li"), current = null;
   
-  // get new and old index
-  let oldIndex = e.dataTransfer.getData('text/plain')
-  let target = $(e.target)
-  let newIndex = target.index()
+    // (B) MAKE ITEMS DRAGGABLE + SORTABLE
+    for (let i of items) {
+      // (B1) ATTACH DRAGGABLE
+      i.draggable = true;
+      
+      // (B2) DRAG START - YELLOW HIGHLIGHT DROPZONES
+      i.ondragstart = (ev) => {
+        current = i;
+        for (let it of items) {
+          if (it != current) { it.classList.add("hint"); }
+        }
+      };
+      
+      // (B3) DRAG ENTER - RED HIGHLIGHT DROPZONE
+      i.ondragenter = (ev) => {
+        if (i != current) { i.classList.add("active"); }
+      };
   
-  // remove dropped items at old place
-  let dropped = $(this).parent().children().eq(oldIndex).remove()
-
-  // insert the dropped items at new place
-  if (newIndex < oldIndex) {
-    target.before(dropped)
-  } else {
-    target.after(dropped)
+      // (B4) DRAG LEAVE - REMOVE RED HIGHLIGHT
+      i.ondragleave = () => {
+        i.classList.remove("active");
+      };
+  
+      // (B5) DRAG END - REMOVE ALL HIGHLIGHTS
+      i.ondragend = () => { for (let it of items) {
+          it.classList.remove("hint");
+          it.classList.remove("active");
+      }};
+   
+      // (B6) DRAG OVER - PREVENT THE DEFAULT "DROP", SO WE CAN DO OUR OWN
+      i.ondragover = (evt) => { evt.preventDefault(); };
+   
+      // (B7) ON DROP - DO SOMETHING
+      i.ondrop = (evt) => {
+        evt.preventDefault();
+        if (i != current) {
+          let currentpos = 0, droppedpos = 0;
+          for (let it=0; it<items.length; it++) {
+            if (current == items[it]) { currentpos = it; }
+            if (i == items[it]) { droppedpos = it; }
+          }
+          if (currentpos < droppedpos) {
+            i.parentNode.insertBefore(current, i.nextSibling);
+          } else {
+            i.parentNode.insertBefore(current, i);
+          }
+        }
+      };
+    }
   }
-}
-
-function cancelDefault (e) {
-  e.preventDefault()
-  e.stopPropagation()
-  return false
-}
