4a007c65faca81bbb23dea719530fee6f5818126
PrzemekG <przemek@gabrych.com.pl>
2022-12-14 22:16:10 +0100

    ert

--- lab5/engine.js
+++ lab5/engine.js
@@ -1,3 +1,6 @@
+
+
+
 window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
 window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction || {READ_WRITE: "readwrite"};
 window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
@@ -8,33 +11,57 @@ if (!window.indexedDB) {
 
 function init(){
 
-var request = window.indexedDB.open("MyTestDatabase", 3);
+  //Łącze się do IndexDB
+  var request = window.indexedDB.open("MyDatabase", 3);
 
-request.onerror = function(event) {
-  alert("Error faced while opening database");
-};
-request.onsuccess = function(event) {
-
-  db = event.target.result;
+  request.onerror = function(event) {
+    alert("Error faced while opening database");
+  };
+  request.onsuccess = function(event) {
   
-
-  db.onerror = function(event) {
-
-	alert("Database error: " + event.target.errorCode);
-  alert("Sprawdź czy nie ma już klienta o podanym peselu!");
+    db = event.target.result;
+    
+  
+    db.onerror = function(event) {
+  
+    alert("Database error: " + event.target.errorCode);
+    alert("Sprawdź czy nie ma już klienta o podanym peselu!");
+    };
+  
   };
 
+function randomString(arr) {
+  let randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex];
+}
+document.getElementById('RandomDataButton').onclick = function(e)
+{
+  document.getElementById('nameInput').value = randomString(['Bartek','Stefan','Marcin','Jerzy','Piotrek', 'Maciej', 'Janek', 'Paweł','Kasia']);
+  document.getElementById('lastNameInput').value = randomString(['Kowalski','Gabrych','Marcinkowski','Stasiak','Kazimierczyk', 'Dobrowolski', 'Marciniak', 'Chałubek','Koza']);
+  document.getElementById('adresInput').value = randomString(['Sloneczna 45','Malinowa 0','Zrodlana 10','Pomaranczowa 50','Wroblewskiego 32', 'Brukowa 15', 'Debowa 2']);
+  document.getElementById('PeselInput').value = (Math.floor(Math.random() * 99999999999 + 10)).toString();
+  document.getElementById('PhoneInput').value = (Math.floor(Math.random() * 999999999 + 10)).toString();
+  document.getElementById('MailInput').value = randomString(['asdf@gmail.com','WGWEdf@onet.com','html@gmail.com','Jacek@wp.com','Stefano@outlook.com', 'Franklin@gmail.com']);
+
 };
+
+
 request.onupgradeneeded = function(event) { 
 
   var db = event.target.result;
-  var objectStore = db.createObjectStore("books", { keyPath: "isbn" });
+  var objectStore = db.createObjectStore("users", { keyPath: "pesel" });
 
   objectStore.createIndex("name", "name", {unique:false});
 
-  objectStore.createIndex("author", "author", {unique:false});
+  objectStore.createIndex("lastname", "lastname", {unique:false});
 
-  objectStore.createIndex("year", "year", {unique:false});
+  objectStore.createIndex("adres", "adres", {unique:false});
+
+  objectStore.createIndex("pesel", "pesel", {unique:false});
+
+  objectStore.createIndex("phone", "phone", {unique:false});
+
+  objectStore.createIndex("mail", "mail", {unique:false});
 
   objectStore.transaction.oncomplete = function(event) {
 	
@@ -44,18 +71,22 @@ request.onupgradeneeded = function(event) {
 document.getElementById('addButton').onclick = function(e) {
 
   var bname = document.getElementById('nameInput').value;
-  var bauthor = document.getElementById('lastNameInput').value;
-  var byear = document.getElementById('adresInput').value;
-  var bisbn = document.getElementById('PeselInput').value;
+  var blastname = document.getElementById('lastNameInput').value;
+  var badres = document.getElementById('adresInput').value;
+  var bpesel = document.getElementById('PeselInput').value;
+  var bphone = document.getElementById('PhoneInput').value;
+  var bmail = document.getElementById('MailInput').value;
   
-  const book_item = {
+  const users_item = {
 	name: bname,
-	author: bauthor,
-	year: byear,
-	isbn: bisbn
+	lastname: blastname,
+	adres: badres,
+	pesel: bpesel,
+  phone: bphone,
+  mail: bmail
   }
 
-  var transaction = db.transaction(["books"], "readwrite");
+  var transaction = db.transaction(["users"], "readwrite");
 
   transaction.oncomplete = function(event) {
 	console.log("all done with transaction");
@@ -65,8 +96,8 @@ document.getElementById('addButton').onclick = function(e) {
 	console.dir(event);
   };
 
-  var booksObjectStore = transaction.objectStore("books");
-  var request = booksObjectStore.add(book_item);
+  var usersObjectStore = transaction.objectStore("users");
+  var request = usersObjectStore.add(users_item);
 
   request.onsuccess = function(event){
 	console.log("added item");
@@ -76,24 +107,13 @@ document.getElementById('addButton').onclick = function(e) {
 
 };
 
-document.getElementById('delButton').onclick = function(e){
-
-  var isbn_del = document.getElementById('isPeselInput').value;
-
-  var request = db.transaction(["books"], "readwrite").objectStore("books").delete(isbn_del);
-
-  request.onsuccess = function(event){
-	console.log(isbn_del+" deleted");
-  };
-
-  updatetable();
-};
 
 function updatetable(){
 
-  document.getElementById("books-table-body").innerHTML = "";
+  
+  document.getElementById("ClientTable").innerHTML = "";
 
-  var request = db.transaction("books").objectStore("books").openCursor();
+  var request = db.transaction("users").objectStore("users").openCursor();
 
   request.onerror = function(event){
 	console.dir(event);
@@ -104,11 +124,25 @@ function updatetable(){
 	cursor = event.target.result;
 
 	if(cursor) {
-	  document.getElementById("books-table-body").innerHTML += "<tr><td>" + cursor.value.name + "</td><td>"
-		+ cursor.value.author + "</td><td>" + cursor.value.year + "</td><td>" + cursor.key + "</td></tr>";
 
+    let but = document.createElement("button");
+    let td = document.getElementById("ClientTable");
+    but.innerText = "Usun";
+    but.className = "btn_buy";
+    but.id = cursor.key;
+    var id = cursor.key;
+	  document.getElementById("ClientTable").innerHTML += "<tr><td>" + cursor.value.name + "</td><td>"
+		+ cursor.value.lastname + "</td><td>" + cursor.value.adres + "</td><td>" + cursor.value.pesel + "</td><td>" 
+    + cursor.value.phone +  "</td><td>" + cursor.value.mail + "</td><td>"
+    + "<button type='button' id='id' >Usun</button>"+ "</td><td>" + "<button type='button' >Edytuj</button>";
+
+    //document.getElementById(cursor.key).onclick = "dilejt()";
+    //but.onclick = function() { dilejt()};
+    //document.getElementById("books-table-body").appendChild(but);
 		cursor.continue();
 	}
   };
 }
+
 }
+
